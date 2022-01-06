a71bf3d1aab1e592fa5bf11788c9d7213a65eb39
ripx80 <ripx80@protonmail.com>
2022-01-06 11:22:33 +0100

    add base site

---
+++ assets/js/custom.js
@@ -0,0 +1,17 @@
+document.onload = setCookie();
+function setCookie() {
+    console.log("setcookie exec")
+    const cookies = document.getElementById("cookies");
+    const cookiesBtn = document.getElementById("cookies-btn");
+    if (!document.cookie) {
+        console.log("no cookie found")
+        cookies.style.display = "block";
+    }
+    cookiesBtn.onclick = function () {
+        console.log("onclick btn")
+        date = new Date();
+        let expr = date.setDate(date.getDate() + 1);
+        document.cookie = "cookies=1; expires=$expr";
+        cookies.style.display = "none";
+    }
+}
