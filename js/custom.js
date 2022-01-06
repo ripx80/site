123fc23aa05669ce5b031747e4a6c0ae293b1cd2
ripx80 <ripx80@users.noreply.github.com>
2022-01-06 11:02:06 +0000

    deploy: 1aa89dffe37b946cfd0592d55e90d96fc6dca1b2

--- js/custom.js
+++ js/custom.js
@@ -1,14 +1,11 @@
 document.onload = setCookie();
 function setCookie() {
-    console.log("setcookie exec")
     const cookies = document.getElementById("cookies");
     const cookiesBtn = document.getElementById("cookies-btn");
     if (!document.cookie) {
-        console.log("no cookie found")
         cookies.style.display = "block";
     }
     cookiesBtn.onclick = function () {
-        console.log("onclick btn")
         date = new Date();
         let expr = date.setDate(date.getDate() + 1);
         document.cookie = "cookies=14; expires=$expr";
